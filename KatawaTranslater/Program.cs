31c1755eba2827e3e11fef33cac7fbc3d0b4e1e3
xyh951115 <471561811@qq.com>
2014-08-03 01:44:38 +0800

    1.3FirstRelease2

---
+++ KatawaTranslater/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace KatawaTranslater
+{
+    static class Program
+    {
+        public static string OpenPath;
+        public static string SettingPath;
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                OpenPath = args[0];
+            }
+            catch
+            {
+                OpenPath = "";
+            }
+            SettingPath = Application.UserAppDataPath + "\\Settings.bin";
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
